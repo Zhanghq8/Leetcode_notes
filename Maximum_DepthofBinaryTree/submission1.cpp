cfd196559c9de78e537df69a7f7b91bc88e03609
Zhanghq8 <hzhang8@wpi.edu>
2019-03-12 13:39:19 -0400

    updateclass

--- Maximum_DepthofBinaryTree/submission1.cpp
+++ Maximum_DepthofBinaryTree/submission1.cpp
@@ -1,51 +1,20 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        int result = 0;
-        if (needle.size() == 0) {
-            return result;
+    int maxDepth(TreeNode* root) {
+        if (root == NULL) {
+            return 0;
         }
-        else {
-            if (haystack.size() >= needle.size()) {
-                int temp = -1;
-                for (int i = 0; i < haystack.size()-needle.size()+1; i++) {
-                    for (int j = 0; j < needle.size(); j++) {
-                        if (haystack[i+j] != needle[j]) {
-                            break;
-                        }
-                        else {
-                            temp = j;
-                        }
-                    }
-                    if (temp == needle.size() - 1) {
-                        result = i;
-                        return result;
-                    }
-                    else {
-                        result = -1;
-                    }
-                }
-            }
-            else {
-                result = -1;
-            }
-        }
-        return result;
+        int hleft = maxDepth(root->left);
+        int hright = maxDepth(root->right);
+        return max(hleft, hright) + 1;
     }
 };
-
-int main()
-{
-    string str1 = "hello";
-    string str2 = "ll";
-    Solution strS;
-    int temp = strS.strStr(str1, str2);
-    cout << temp << " \n";
-    
-}
-
