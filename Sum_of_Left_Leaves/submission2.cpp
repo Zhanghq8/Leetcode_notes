7df43228c8a4160b1f76f769ee60c0048a501e49
Zhanghq8 <hzhang8@wpi.edu>
2019-05-09 13:17:19 -0400

    add recursive solution

---
+++ Sum_of_Left_Leaves/submission2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if (root == NULL)
+        {
+            return 0;
+        }
+        int sum = 0;
+        if (root->left != NULL)
+        {
+            if (root->left->left==NULL && root->left->right==NULL)
+            {
+                sum += root->left->val;
+            }
+            else
+            {
+                sum += sumOfLeftLeaves(root->left);
+            }
+        }
+        if (root->right != NULL)
+        {
+            sum += sumOfLeftLeaves(root->right);
+        }
+        return sum;
+    }
+};
