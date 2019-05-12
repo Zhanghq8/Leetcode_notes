66241f484ca6b8e76bcd264999c79e52108da854
Zhanghq8 <hzhang8@wpi.edu>
2019-05-12 01:38:01 -0400

    add iterative solution

---
+++ Binary_Tree_Preorder_Traversal/submission2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        if (root == NULL) {
+            return result;
+        }
+        stack<TreeNode*> S;
+        TreeNode* current = root;
+        while (!S.empty() || current) {
+            while (current) {
+                result.push_back(current->val);
+                S.push(current);
+                current = current->left;
+            }
+            current = S.top();
+            S.pop();
+            current = current->right;
+        }
+        return result;
+    }
+};
