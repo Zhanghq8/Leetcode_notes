98af8c498c218568aa08b7b4cb4c8315c168c708
Zhanghq8 <hzhang8@wpi.edu>
2019-05-19 14:58:48 -0400

    add solution

--- Combination_Sum_II/submission2.cpp
+++ Combination_Sum_II/submission2.cpp
@@ -14,27 +14,17 @@ public:
     }
 private:
     void addsolution(vector<vector<int>>& result, vector<int>& output, vector<int>& candidates, int index, int target) {
-        
         if (target < 0 ) {
             return;
         }
         if (target == 0) {
-            bool same = false;
-            for (auto n:result) {
-                if (n != output) {
-                    same = false;
-                }
-                else {
-                    same = true;
-                    break;
-                }
-            }
-            if (same == false) {
-                result.push_back(output);
-            }
+            result.push_back(output);
             return;
         }
         for (int i=index; i<candidates.size(); i++) {
+            if (i > index && candidates[i] == candidates[i-1]) {
+                continue;   
+            }
             output.push_back(candidates[i]);
             addsolution(result, output, candidates, i+1, target-candidates[i]);
             output.pop_back();
