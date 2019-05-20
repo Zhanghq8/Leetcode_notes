5d89d5d1aa7a47eab25d71b06c209641d5c19cff
Zhanghq8 <hzhang8@wpi.edu>
2019-05-20 02:12:48 -0400

    No. 77

---
+++ Combinations/submission1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> result;
+        vector<int> output;
+        addsolution(n, k, 1, output, result);
+        return result;
+    }
+private:
+    void addsolution(int max_num, int num, int start, vector<int>& output, vector<vector<int>>& result) {
+        if (output.size() == num) {
+            result.push_back(output);
+            return;
+        }
+        for (int i=start; i<=max_num; i++) {
+            output.push_back(i);
+            addsolution(max_num, num, i+1, output, result);
+            output.pop_back();
+        }
+    }
+};
