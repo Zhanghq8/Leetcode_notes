450fc41b309dd88441889eef1de590883b87d64d
Zhanghq8 <hzhang8@wpi.edu>
2019-05-21 15:07:07 -0400

    No. 152

--- Maximum_Product_Subarray/submission1.cpp
+++ Maximum_Product_Subarray/submission1.cpp
@@ -5,14 +5,23 @@ using namespace std;
 
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
-        int length = nums.size()-1;
-        for (int i=length; i>=0; i--) {
-            if (nums[i]+i >= length) {
-                length = i;
+    int maxProduct(vector<int>& nums) {
+        if (nums.size() == 0) {
+            return 0;
+        }
+        int result = nums[0];
+        vector<int> dp_max(nums.size());
+        vector<int> dp_min(nums.size());
+        dp_max[0] = nums[0];
+        dp_min[0] = nums[0];
+        for (int i=1; i<nums.size(); i++) {
+            dp_max[i] = max(nums[i], max(dp_max[i-1]*nums[i], dp_min[i-1]*nums[i]));
+            dp_min[i] = min(nums[i], min(dp_min[i-1]*nums[i], dp_max[i-1]*nums[i]));
+            if (result < dp_max[i]) {
+                result = dp_max[i];
             }
         }
-        return length == 0;
+        return result;
     }
 };
 
