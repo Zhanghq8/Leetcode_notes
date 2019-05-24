c98b6f6a12983055e0ce561f8f64c245277d3563
Zhanghq8 <hzhang8@wpi.edu>
2019-05-24 14:45:46 -0400

    update BM algorithm

---
+++ Majority_Element_II/submission2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// 
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int m_ele1 = 0;
+        int m_ele2 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        vector<int> result;
+        for (int i=0; i<nums.size(); i++) {
+            if (nums[i] == m_ele1) {
+                count1++;
+            }
+            else if (nums[i] == m_ele2) {
+                count2++;
+            }
+            else if (nums[i] != m_ele1 && count1 == 0) {
+                count1++;
+                m_ele1 = nums[i];
+            }
+            else if (nums[i] != m_ele2 && count2 == 0) {
+                count2++;
+                m_ele2 = nums[i];
+            }
+            else {
+                count1--;
+                count2--;
+            }
+        }
+        count1 = count2 = 0;
+        for (int i=0; i<nums.size(); i++) {
+            if (nums[i] == m_ele1) {
+                count1++;
+            }
+            else if (nums[i] == m_ele2) {
+                count2++;
+            }
+        }
+        if (count1 > nums.size()/3) {
+            result.push_back(m_ele1);
+        }
+        if (count2 > nums.size()/3) {
+            result.push_back(m_ele2);
+        }
+        return result;
+    }
+
+};
