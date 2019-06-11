0dbd8fc7ebe6ddb8550ee148b96561d611b9b3ac
Zhanghq8 <hzhang8@wpi.edu>
2019-06-11 12:07:48 -0400

    No. 326

---
+++ Power_of_Three/submission2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        while (n > 0 && n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
+};
+
