bfb1880e2a95620854f7c5f0087681f2993bf17a
Zhanghq8 <hzhang8@wpi.edu>
2019-08-02 18:28:04 -0400

    No. 263

---
+++ Ugly_Number/README.md
@@ -0,0 +1,27 @@
+## Question
+
+Write a program to check whether a given number is an ugly number.
+
+Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+
+# Example1:
+```
+nput: 6
+Output: true
+Explanation: 6 = 2 × 3
+```
+# Example2:
+```
+Input: 8
+Output: true
+Explanation: 8 = 2 × 2 × 2
+```
+# Example3:
+```
+Input: 14
+Output: false 
+Explanation: 14 is not ugly since it includes another prime factor 7.
+```
+# Note:
+- 1 is typically treated as an ugly number.
+- Input is within the 32-bit signed integer range: [−231,  231 − 1].
---
+++ Ugly_Number/submission1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isUgly(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        while (num >= 2) {
+            if (num % 2 == 0) {
+                num /= 2;
+            }
+            else if (num % 3 == 0) {
+                num /= 3;
+            }
+            else if (num % 5 == 0) {
+                num /= 5;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+};
