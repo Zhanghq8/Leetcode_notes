f5173507b21cdeeaf4804242a65158396b110ddd
Zhanghq8 <hzhang8@wpi.edu>
2019-08-29 00:10:11 -0400

    No. 701

---
+++ Insert_into_a_Binary_Search_Tree/submission1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if (root == NULL) {
+            root = new TreeNode(val);
+            return root;
+        }
+        if (val > root->val) {
+            root->right = insertIntoBST(root->right, val);
+        }
+        else {
+            root->left = insertIntoBST(root->left, val);
+        }
+        return root;
+    }
+};
