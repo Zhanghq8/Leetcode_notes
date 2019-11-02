8ff1e98c92aa1e0333f7d6218c88ac207b54f22b
Zhanghq8 <hzhang8@wpi.edu>
2019-11-02 17:39:59 -0400

    Recursion

---
+++ 24_Swap_Nodes_in_Pairs/submission2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (!head || !head->next) {
+            return head;
+        }
+        ListNode* temp = head->next;
+        head->next = swapPairs(head->next->next);
+        temp->next = head;
+        return temp;
+    }
+};
