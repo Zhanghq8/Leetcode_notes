bd5c00cab4d420626dbd4193def8b17dce7c0179
Zhanghq8 <hzhang8@wpi.edu>
2019-11-02 22:04:15 -0400

    DFS

---
+++ 133_Clone_Graph/submission1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        unordered_map<Node*, Node*> umap;
+        return dfs(node, umap);
+    }
+private:
+    Node* dfs (Node* node, unordered_map<Node*, Node*>& umap) {
+        if (node == NULL) {
+            return node;
+        }
+        if (umap.find(node) != umap.end()) {
+            return umap[node];
+        }
+        Node* clone = new Node(node->val);
+        umap[node] = clone;
+        for (auto neighbor : node->neighbors) {
+            clone->neighbors.push_back(dfs(neighbor, umap));
+        }
+        return clone;
+    }
+};
