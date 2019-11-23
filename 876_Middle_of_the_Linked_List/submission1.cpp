57cc617faf40ac74fb1e2b893182bb5cc44b2707
Zhanghq8 <hzhang8@wpi.edu>
2019-11-23 15:39:48 -0500

    slow and fast pointer

---
+++ 876_Middle_of_the_Linked_List/submission1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if (!head) {
+            return head;
+        }
+        ListNode* dummyNode = new ListNode(-1);
+        dummyNode->next = head;
+        ListNode* slowPtr = dummyNode;
+        ListNode* fastPtr = dummyNode;
+        while (fastPtr->next && fastPtr->next->next) {
+            slowPtr = slowPtr->next;
+            fastPtr = fastPtr->next->next;
+        }
+        return slowPtr->next;
+    }
+};
