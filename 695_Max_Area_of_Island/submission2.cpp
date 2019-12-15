dfcfe1fe78890dc39c9c0a1c9e9d81d0b80cce4b
Zhanghq8 <hzhang8@wpi.edu>
2019-12-14 21:13:41 -0500

    BFS

--- 695_Max_Area_of_Island/submission2.cpp
+++ 695_Max_Area_of_Island/submission2.cpp
@@ -6,31 +6,34 @@ using namespace std;
 class Solution {
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int max_area = INT_MIN;
-        vector<vector<bool>> visited{grid.size(), vector<bool>(grid[0].size(), false)};
+        int max_area = 0;
+        vector<vector<int>> dirs{{-1,0}, {1,0}, {0,-1}, {0,1}};
         for (int i=0; i<grid.size(); i++) {
             for (int j=0; j<grid[i].size(); j++) {
-                if (grid[i][j] == 1 && !visited[i][j]) {
-                    int area = 0;
-                    dfs(grid, visited, i, j, area);
-                    if (area > max_area) {
-                        max_area = area;
+                if (grid[i][j] == 1) {
+                    int area = 1;
+                    queue<pair<int, int>> Q;
+                    Q.push({i,j});
+                    while (!Q.empty()) {
+                        auto cur = Q.front();
+                        grid[cur.first][cur.second] = 2;
+                        Q.pop();
+                        for (auto n : dirs) {
+                            int x = cur.first + n[0];
+                            int y = cur.second + n[1];
+                            if (x<0 || x>=grid.size() || y<0 || y>=grid[x].size() 
+                                || grid[x][y] != 1) {
+                                continue;
+                            }
+                            area++;
+                            grid[x][y] = 2;
+                            Q.push({x,y});
+                        }
                     }
+                    max_area = max(area, max_area);
                 }
             }
         }
-        return max_area == INT_MIN ? 0 : max_area;
-    }
-private:
-    void dfs (vector<vector<int>>& grid, vector<vector<bool>>& visited, int x, int y, int& area) {
-        if (x<0 || x>=grid.size() || y<0 || y>=grid[x].size() || visited[x][y] || !grid[x][y]) {
-            return;
-        }
-        area++;
-        visited[x][y] = true;
-        dfs(grid, visited, x-1, y, area);
-        dfs(grid, visited, x+1, y, area);
-        dfs(grid, visited, x, y-1, area);
-        dfs(grid, visited, x, y+1, area);
+        return max_area;
     }
 };
