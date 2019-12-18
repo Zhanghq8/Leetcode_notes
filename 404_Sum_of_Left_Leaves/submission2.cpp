7bbb53e12153c413b38f72eff96351024dee7699
Zhanghq8 <hzhang8@wpi.edu>
2019-12-18 01:48:05 -0500

    Recursion

---
+++ 404_Sum_of_Left_Leaves/submission2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+        if (root->left && !root->left->left && !root->left->right) {
+            return root->left->val + sumOfLeftLeaves(root->right);
+        }
+        return sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+    }
+};
