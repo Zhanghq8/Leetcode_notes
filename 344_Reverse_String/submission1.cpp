2158ed7edb4d24fb4494029e7a912466ae1031ec
Zhanghq8 <hzhang8@wpi.edu>
2019-12-18 13:58:20 -0500

    Iteration

--- 344_Reverse_String/submission1.cpp
+++ 344_Reverse_String/submission1.cpp
@@ -7,14 +7,21 @@ using namespace std;
 class Solution {
 public:
     void reverseString(vector<char>& s) {
+        if (s.empty()) {
+            return;
+        }
         int left = 0;
         int right = s.size()-1;
-        while (left < right) {
-            char temp = s[right];
-            s[right] = s[left];
-            s[left] = temp;
+        while (left <= right) {
+            helper(s[left], s[right]);
             left++;
             right--;
         }
+    }
+private:
+    void helper(char& a, char& b) {
+        char temp = a;
+        a = b;
+        b = temp;
     }
 };
