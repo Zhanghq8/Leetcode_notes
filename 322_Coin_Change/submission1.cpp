b929e5b44662c643f3d79acd5fb836ca8a3fcb51
Zhanghq8 <hzhang8@wpi.edu>
2020-01-02 16:54:45 -0500

    2d DP

---
+++ 322_Coin_Change/submission1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// DFS
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        sort(coins.rbegin(), coins.rend());
+        int result = INT_MAX;
+        helper(coins, 0, amount, 0, result);
+        return result == INT_MAX ? -1 : result;
+    }
+private:
+    void helper(vector<int>& coins, int index, int amount, int count, int& result) {
+        if (amount == 0) {
+            result = min(count, result);
+        }
+        if (index == coins.size()) {
+            return;
+        }
+        for (int i=amount/coins[index]; i>=0 && count + i < result; i--) {
+            helper(coins, index+1, amount-i*coins[index], count+i, result);
+        }
+    }
+};
