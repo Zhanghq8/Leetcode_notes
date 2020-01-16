1973cc8a916d329f3f2e7d34e81f0608b1de4b53
Zhanghq8 <hzhang8@wpi.edu>
2020-01-16 14:38:58 -0500

    two pointers

--- 26_Remove_Duplicates_from_Sorted_Array/submission1.cpp
+++ 26_Remove_Duplicates_from_Sorted_Array/submission1.cpp
@@ -4,48 +4,24 @@
 
 using namespace std;
 
-// slow pointer 0
-// fast pointer 1
-// Traverse the vector by fast pointer, check if the element at slow ptr is equal to the
-// fast pointer. If so, fast ptr++, otherwise, copy the value at fast ptr to slow ptr + 1
-// and fast ptr++
-
-
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.size() == 0) {
+        if (nums.empty()) {
             return 0;
         }
-        int index = 0;
-        for (int i=1; i<nums.size(); i++) {
-            if (nums[index] != nums[i]) {
-                index++;
-                nums[index] = nums[i];
+        int slow = 1;
+        int fast = 1;
+        int size = nums.size();
+        while (slow < size && fast < size) {
+            if (nums[slow-1] == nums[fast]) {
+                fast++;
+            } else {
+                nums[slow] = nums[fast];
+                slow++;
+                fast++;
             }
         }
-        return index+1;
+        return slow;
     }
 };
-
-// class Solution {
-// public:
-//     int removeDuplicates(vector<int>& nums) {
-//         if (nums.size() <= 1) {
-//             return nums.size();
-//         }
-//         int slow = 0;
-//         int fast = 1;
-//         while (slow < nums.size() && fast < nums.size()) {
-//             if (nums[slow] == nums[fast]) {
-//                 fast++;
-//             }
-//             else {
-//                 slow++;
-//                 nums[slow] = nums[fast];
-//                 fast++;
-//             }
-//         }
-//         return slow+1;
-//     }
-// };
