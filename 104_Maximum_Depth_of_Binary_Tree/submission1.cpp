fb84e87e371cf168f2817b2c43482424407ea83a
Zhanghq8 <hzhang8@wpi.edu>
2020-01-24 22:20:15 -0500

    tree + recursion

---
+++ 104_Maximum_Depth_of_Binary_Tree/submission1.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+        int leftHeight = maxDepth(root->left);
+        int rightHeight = maxDepth(root->right);
+        return max(leftHeight, rightHeight) + 1;
+    }
+};
