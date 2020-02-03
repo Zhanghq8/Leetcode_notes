e6de6c6bcc256351a19f866344f3d779cd6399dc
Zhanghq8 <hzhang8@wpi.edu>
2020-02-03 16:34:39 -0500

    DFS

--- 17_Letter_Combinations_of_a_Phone_Number/submission1.cpp
+++ 17_Letter_Combinations_of_a_Phone_Number/submission1.cpp
@@ -13,7 +13,7 @@ public:
             return {};
         }
         vector<string> result;
-        vector<string> dict = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        
         helper(result, "", dict, digits, 0);
         return result;
     }
@@ -26,6 +26,32 @@ private:
         string temp = dict[digits[level] - '2'];
         for (int i=0; i<temp.size(); i++) {
             helper(result, solution+temp[i], dict, digits, level+1);
+        }
+    }
+};
+
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) {
+            return {};
+        }
+        vector<string> dict = {" ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> result;
+        string solution = "";
+        helper(digits, 0, dict, solution, result);
+        return result;
+    }
+private:
+    void helper(string digits, int length, vector<string>& dict, string solution, vector<string>& result) {
+        if (length == digits.size()) {
+            result.push_back(solution);
+            return;
+        }
+        for (char c : dict[digits[length] - '0']) {
+            solution.push_back(c);
+            helper(digits, length + 1, dict, solution, result);
+            solution.pop_back();
         }
     }
 };
