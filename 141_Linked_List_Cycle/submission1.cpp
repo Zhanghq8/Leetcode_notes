0020af83364f275a9ebeef4393394810d8a3aca2
Zhanghq8 <hzhang8@wpi.edu>
2020-02-18 15:19:43 -0500

    two pointers

--- 141_Linked_List_Cycle/submission1.cpp
+++ 141_Linked_List_Cycle/submission1.cpp
@@ -14,18 +14,18 @@ using namespace std;
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if (!head || !head->next) {
-             return false;
+        if (!head) {
+            return false;
         }
-        ListNode* slowPtr = head;
-        ListNode* fastPtr = head->next;
-        while (slowPtr != fastPtr) {
-            if (!fastPtr || !fastPtr->next) {
-              return false;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                return true;
             }
-            slowPtr = slowPtr->next;
-            fastPtr = fastPtr->next->next;
         }
-        return true;
+        return false;
     }
 };
