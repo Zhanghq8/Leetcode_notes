c10ee5110fa8453747a5b43063c662f6bfc32ea4
Zhanghq8 <hzhang8@wpi.edu>
2020-02-28 13:59:40 -0500

    DFS

--- 17_Letter_Combinations_of_a_Phone_Number/submission1.cpp
+++ 17_Letter_Combinations_of_a_Phone_Number/submission1.cpp
@@ -9,30 +9,6 @@ using namespace std;
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        if (digits.empty() == true) {
-            return {};
-        }
-        vector<string> result;
-        
-        helper(result, "", dict, digits, 0);
-        return result;
-    }
-private:
-    void helper(vector<string>& result, string solution, vector<string>& dict, string digits, int level) {
-        if (level == digits.size()) {
-            result.push_back(solution);
-            return;
-        }
-        string temp = dict[digits[level] - '2'];
-        for (int i=0; i<temp.size(); i++) {
-            helper(result, solution+temp[i], dict, digits, level+1);
-        }
-    }
-};
-
-class Solution {
-public:
-    vector<string> letterCombinations(string digits) {
         if (digits.empty()) {
             return {};
         }
