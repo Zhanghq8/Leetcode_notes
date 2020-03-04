afd3a7a294bd346728ee4ac3d4ca2dc5856a136c
Zhanghq8 <hzhang8@wpi.edu>
2020-03-04 00:28:04 -0500

    DFS

---
+++ 90_Subsets_II/submission2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        if (nums.empty()) {
+            nums;
+        }
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
+        vector<int> output;
+        helper(result, nums, output, 0);
+        return result;
+    }
+private: 
+    void helper(vector<vector<int>>& result, vector<int>& nums, vector<int>& output, int index) {
+        if (index == nums.size()) {
+            result.emplace_back(output);
+            return;
+        }
+        output.push_back(nums[index]);
+        helper(result, nums, output, index+1);
+        output.pop_back();
+        while (index < nums.size() && nums[index+1] == nums[index]) {
+            index++;
+        }
+        helper(result, nums, output, index+1);
+    }
+};
+
