eb5867531290f4f5635628e6f4f37a565fb3ddfc
Zhanghq8 <hzhang8@wpi.edu>
2020-03-25 17:04:57 -0400

    1d DP

---
+++ 198_House_Robber/submission1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+        vector<int> dp(nums.size() + 1, 0);
+        dp[1] = nums[0];
+        for (int i = 2; i < nums.size() + 1; i++) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+        }
+        return dp.back();
+    }
+};
