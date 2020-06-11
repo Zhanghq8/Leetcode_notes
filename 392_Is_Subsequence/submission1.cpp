022c9eaecc03082f2f7e6b65851e4b0ff918b2f1
Zhanghq8 <hzhang8@wpi.edu>
2020-06-11 15:29:53 -0400

    math

---
+++ 392_Is_Subsequence/submission1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int sIndex = 0;
+        int tIndex = 0;
+        while (sIndex < s.size() && tIndex < t.size()) {
+            if (s[sIndex] == t[tIndex]) {
+                sIndex++;
+                tIndex++;
+            } else {
+                tIndex++;
+            }
+        }
+        return sIndex == s.size();
+    }
+};
